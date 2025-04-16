c9aebbdc21973ead0fe04a60c177bc203a47c52a
Valentin Chatelet <valentin.chatelet@gmail.com>
2025-04-17 00:49:34 +0200

    Updated slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,56 +1,65 @@
 const gap = 300
-window.slideLeft = () => {
+window.slideLeft = (e) => {
+
+    if(e.target.classList.contains("disable")) return
     const container = document.querySelector(".slideshow")
     const ref = document.querySelector(".slide-element")
     const elems = Array.from(document.querySelectorAll(".slide-element"));
     const activeEl = elems.filter(e => e.classList.contains("active"))[0];
-    const totalSize = (elems.length * ref.offsetWidth) + elems.length * container.style.gap;
-
+    const totalSize = (elems.length * ref.offsetWidth) + elems.length * gap;
+    const rightBtn = document.querySelector(".arrow-right")
     const leftBtn = document.querySelector(".arrow-left")
 
-    if(container.offsetLeft*-1 < ref.offsetWidth){
+    if(Math.abs(container.offsetLeft) < ref.offsetWidth || elems.indexOf(activeEl) === 0){
         leftBtn.classList.add("disable")
     }
 
-    if(container.offsetLeft*-1 < totalSize){
+    if(Math.abs(container.offsetLeft) < totalSize){
         if(elems.indexOf(activeEl)-1 > 0){
             const nextId =  elems.indexOf(activeEl) - 1
             container.style.marginLeft = -((ref.offsetWidth * nextId) + gap*nextId)+ "px"
             activeEl.classList.remove("active")
             elems[nextId].classList.add("active")
             if(leftBtn.classList.contains("disable")) leftBtn.classList.remove("disable")
-
+            if(nextId+1 < elems.length && rightBtn.classList.contains("disable")) rightBtn.classList.remove("disable") 
         }else{
             container.style.marginLeft = 0
             elems.forEach(e => e.classList.remove("active"))
             elems[0].classList.add("active")
+            leftBtn.classList.add("disable")
+            rightBtn.classList.remove("disable")
         }
     }else{
         container.style.marginLeft = 0
         elems[0].classList.add("active")
+        leftBtn.classList.add("disable")
+        rightBtn.classList.remove("disable")
     }
 }
 
-window.slideRight = () => {
+window.slideRight = (e) => {
+    if(e.target.classList.contains("disable")) return
     const container = document.querySelector(".slideshow")
     const ref = document.querySelector(".slide-element")
     const elems = Array.from(document.querySelectorAll(".slide-element"));
     const activeEl = elems.filter(e => e.classList.contains("active"))[0];
     const totalSize = (elems.length * ref.offsetWidth) + elems.length * container.style.gap;
     const leftBtn = document.querySelector(".arrow-left")
-
+    const rightBtn = document.querySelector(".arrow-right")
+    
     if(container.offsetLeft < totalSize){
-        if(elems.indexOf(activeEl)+1 < elems.length){
-            const nextId =  elems.indexOf(activeEl) + 1
+        if(elems.indexOf(activeEl)+1 <= elems.length){
+            const nextId =  elems.indexOf(activeEl) + 1;
+            if(nextId === elems.length-1) rightBtn.classList.add("disable")
             container.style.marginLeft = -((ref.offsetWidth * nextId) + gap*nextId)+ "px"
             activeEl.classList.remove("active")
             elems[nextId].classList.add("active")
             if(leftBtn.classList.contains("disable")) leftBtn.classList.remove("disable")
-
         }else{
             container.style.marginLeft = 0
             leftBtn.classList.add("disable")
             elems[0].classList.add("active")
+            activeEl.classList.remove("active")
         }
     }else{
         container.style.marginLeft = 0
